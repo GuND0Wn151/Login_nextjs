d2c563a1e6092905c91b710c713b1ed86e916d29
Mahesh <kmaheshgupta151@outlook.com>
2022-07-15 17:57:23 +0530

    Fixed Warnings

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from 'next/link'
+/* eslint-disable @next/next/no-img-element */
+
 import Navbar from "../utils/Navbar";
 // const connectMongo = async () =>
 //     mongoose.connect(
@@ -12,95 +15,108 @@ import Navbar from "../utils/Navbar";
 // );
 //const connectMongo = async () => mongoose.connect(process.env.MONGO_URI);
 export default function Home() {
+  
   return (
     <>
-    <div class=" flex flex-auto mb-10 mt-5 ml-10">
-       <div class="flex basis-3/4">
-       <h1 class=" text-2xl mr-4 text-blue-700">NextJsLogin</h1>
-       <h1 class=" text-xl mr-4 mt-1">Home</h1>
-       <h1 class="text-xl mr-4 mt-1">Courses</h1>
+    <div className=" flex flex-auto mb-10 mt-5 ml-10">
+       <div className="flex basis-3/4">
+       <h1 className=" text-2xl mr-4 text-blue-700">NextJsLogin</h1>
+       <h1 className=" text-xl mr-4 mt-1">Home</h1>
+       <h1 className="text-xl mr-4 mt-1">Courses</h1>
         </div> 
-        <div>
-          <h1 class="text-xl mr-4 mt-1">Contact us</h1>
+        <div className="flex">
+          <h1 className="text-xl mt-1 mr-2">
+            <Link href='/login' className="text-2xl mt-1">Login</Link>
+            </h1>
+          <h1 className="text-xl mt-1 ml-3">
+            <Link href='/register' className="text-2xl mt-1 ">Register</Link>
+            </h1>
         </div>
+
     </div>
-    <div class="flex">
-      <div className={styles.container} class="m-30 mt-30 ml-30">
-        <div class="max-w-sm  rounded overflow-hidden shadow-lg">
+    <div className="flex">
+      <div className="m-30 mt-30 ml-30 p-10">
+        <div className="max-w-sm  rounded overflow-hidden shadow-lg">
           <img
-            class="w-full"
+            className="w-full"
             src="https://cdn.pixabay.com/photo/2015/11/15/07/47/geometry-1044090_960_720.jpg"
+            alt="maths"
+             layout="fill"
           ></img>
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Mathamatics</div>
-            <p class="text-gray-700 text-base">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Mathamatics</div>
+            <p className="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
               exercitationem praesentium nihil.
             </p>
           </div>
-          <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #algebra
             </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #trignometry
             </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #geometry
             </span>
           </div>
         </div>
       </div>
-      <div className={styles.container} class="m-30 mt-30 ml-30">
-        <div class="max-w-sm  rounded overflow-hidden shadow-lg">
+      <div  className="m-30 mt-30 ml-30 p-10">
+        <div className="max-w-sm  rounded overflow-hidden shadow-lg">
           <img
-            class="w-full"
+            className="w-full"
             src="https://cdn.pixabay.com/photo/2017/02/01/13/53/analysis-2030265_960_720.jpg"
+            alt='Science'
+            
           ></img>
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Science</div>
-            <p class="text-gray-700 text-base">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Science</div>
+            <p className="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
               exercitationem praesentium nihil.
             </p>
           </div>
-          <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #biology
             </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #physics
             </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #LifeScience
             </span>
           </div>
         </div>
       </div>
-      <div className={styles.container} class="m-30 mt-300 ml-30">
-        <div class="max-w-sm  rounded overflow-hidden shadow-lg">
+      <div  className="m-30 mt-300 ml-30 p-10">
+        <div className="max-w-sm  rounded overflow-hidden shadow-lg">
           <img
-            class="w-full"
+            className="w-full"
             src="https://cdn.pixabay.com/photo/2013/04/08/14/22/stonehenge-101801__340.jpg"
+            alt="social"
+            
           ></img>
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">Social Studies and History</div>
-            <p class="text-gray-700 text-base">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Social Studies and History</div>
+            <p className="text-gray-700 text-base">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Voluptatibus quia, nulla! Maiores et perferendis eaque,
               exercitationem praesentium nihil.
             </p>
           </div>
-          <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #History
             </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #Commerce
             </span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #Economics
             </span>
           </div>
--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Router from "next/router";
+import Link from 'next/link';
 function login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -36,6 +37,7 @@ function login() {
           alert(ress.message);
         } else {
             statuscode=0
+            localStorage.setItem('status',1);
           Router.push("/");
         }
       } catch (err) {
@@ -45,54 +47,54 @@ function login() {
   };
 
   return (
-    <section class="min-h-screen flex flex-col bg-loginpage bg-cover  bg-no-repeat">
-      <div class="flex flex-1 items-center justify-center  ">
-        <div class="rounded-lg sm:border-3 border-gray-400 px-4 lg:px-20 py-16 bg-white  shadow-inner  lg:max-w-xl  w-full text-center">
-          <form class="text-center" onSubmit={handleSubmit}>
-            <h1 class="font-bold tracking-wider text-3xl mb-8 w-full  h-full text-black-600">
+    <section className="min-h-screen flex flex-col bg-loginpage bg-cover  bg-no-repeat">
+      <div className="flex flex-1 items-center justify-center  ">
+        <div className="rounded-lg sm:border-3 border-gray-400 px-4 lg:px-20 py-16 bg-white  shadow-inner  lg:max-w-xl  w-full text-center">
+          <form className="text-center" onSubmit={handleSubmit}>
+            <h1 className="font-bold tracking-wider text-3xl mb-8 w-full  h-full text-black-600">
               Login Page
             </h1>
-            <div class="py-2 text-left">
+            <div className="py-2 text-left">
               <input
                 type="email"
-                class="bg-gray-200 border-3 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg  "
+                className="bg-gray-200 border-3 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg  "
                 placeholder="Email"
                 name="email"
               />
             </div>
-            <div class="py-2 text-left">
+            <div className="py-2 text-left">
               <input
                 type="password"
-                class="bg-gray-100 border-3 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg  "
+                className="bg-gray-100 border-3 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg  "
                 placeholder="Password"
                 name="password"
               />
             </div>
-            <div class="py-2">
+            <div className="py-2">
               <button
                 type="submit"
-                class="border-2 border-gray-100 focus:outline-none bg-green-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg "
+                className="border-2 border-gray-100 focus:outline-none bg-green-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg "
               >
                 Sign In
               </button>
             </div>
           </form>
-          <div class="text-center">
-            <a href="#" class="hover:underline">
+          <div className="text-center">
+            <Link href="#" className="hover:underline">
               Forgot password?
-            </a>
+            </Link>
           </div>
-          <div class="text-center mt-12">
+          <div className="text-center mt-12">
             <span></span>
-            <span>Don't have an account?</span>
+            <span>Dont have an account?</span>
           </div>
-          <div class="text-center">
-            <a
+          <div className="text-center">
+            <Link
               href="/register"
-              class="font-light text-md text-red-600 underline font-semibold "
+              className="font-light text-md text-red-600 underline font-semibold "
             >
               Create One Here..
-            </a>
+            </Link>
           </div>
         </div>
       </div>
