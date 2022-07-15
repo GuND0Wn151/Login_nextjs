e8410ecbdf7ee8f7c1149a7c092664da9bb469b1
Mahesh <kmaheshgupta151@outlook.com>
2022-07-15 18:15:36 +0530

    Fixed Env Variables

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   swcMinify: true,
   images:{
     domains:['cdn.pixabay.com']
+  },
+  env:{
+    MONGO_URL:"mongodb+srv://gundown:gundown@credentials.5vlv3qq.mongodb.net/?retryWrites=true&w=majority"
   }
 }
 
